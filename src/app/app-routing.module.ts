995dc7b5032150209d619f659fca62c5c702cb97
selmaomer <selmasoso@gmail.com>
2024-02-12 12:11:08 +0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {path:'',redirectTo:'home',pathMatch:'full'}, 
+  {path:'',component:HomeComponent,title: 'home'},
+{path:'about',component:AboutComponent,title: 'ABOUT'},
+{path:'portfolio',component:PortfolioComponent,title:'portfolio'},
+{path:'contact',component:ContactComponent,title:'CONTACT'}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    FooterComponent,
+    PortfolioComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
